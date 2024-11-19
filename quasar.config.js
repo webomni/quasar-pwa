0151270b1425ebf5f3ee01400be8e7fcb56f0813
pauloh <phc@sygmasistemas.com.br>
2024-11-19 17:00:17 -0300

    Melhoria nos icon

--- quasar.config.js
+++ quasar.config.js
@@ -124,7 +124,7 @@ module.exports = configure(function (/* ctx */) {
       // extendSSRWebserverConf (esbuildConf) {},
       // extendPackageJson (json) {},
 
-      pwa: false,
+      pwa: true,
 
       // manualStoreHydration: true,
       // manualPostHydrationTrigger: true,
